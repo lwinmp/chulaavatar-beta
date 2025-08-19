e35f30a350ca4cfb06a74a2d5b5f565ae7dbe35a
Lwin <lwinminpaing.mm@gmail.com>
2025-08-19 10:05:44 +0700

    Welcome Page CSS fixed

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -6,77 +6,82 @@ import Avatar4 from './assets/avatar4.png';
 import centerimage from './assets/centerimage.jpg';
 import avatar5 from './assets/avatar5.jpg';
 
-function WelcomePage(){
-    const navigate = useNavigate();
+function WelcomePage() {
+  const navigate = useNavigate();
 
-    const handleButtonClick = () => {
-        navigate('/camera-code', '_blank', 'noopener,noreferrer');
-       // window.open(window.location.origin + '/qr-code', '_blank', 'noopener,noreferrer');
-    };
+  const handleButtonClick = () => {
+    navigate('/camera-code', '_blank', 'noopener,noreferrer');
+  };
 
-    const handleHomeClick = () => {
-      // navigate('/');
-       window.location.href = '/';
-        //window.open(window.location.origin + '/', '_blank', 'noopener,noreferrer');
-    };
+  const handleHomeClick = () => {
+    window.location.href = '/';
+  };
 
-    const handleAboutChulaClick = () => {
-       navigate('/about-chula');
-       //window.location.href = '/';
-        //window.open(window.location.origin + '/', '_blank', 'noopener,noreferrer');
-    };
-    
-    return(
-        <>
-            <div className='welcome-page'>
-                <div className='navbar-container'>
-                    <a className='navbar-chulaavatar-logo' onClick={handleHomeClick}>ChulaAvatar</a>
-                    <ul className='navbar-menu'>
-                        <li className='navbar-item' onClick={handleAboutChulaClick}>About ChulaAvatar</li>
-                        <li className='navbar-item'>MANGOS</li>
-                        <li className='navbar-item'>Contact Us</li>
-                    </ul>
-                </div>
+  const handleAboutChulaClick = () => {
+    navigate('/about-chula');
+  };
 
-                <div className="welcome-container"> 
-                    <h1 className="welcome-h1">Welcome to ChulaAvatar</h1>
-                    <p className="welcome-p">
-                        Experience the top-notch technology with ChulaAvatar. You can create your lifelike-3D Avatar
-                        with one click of your selfie camera.
-                    </p>
-                
+  return (
+    <div className="welcome-page">
+      {/* Navbar */}
+      <div className="navbar-container">
+        <a className="navbar-chulaavatar-logo" onClick={handleHomeClick}>ChulaAvatar</a>
+        <ul className="navbar-menu">
+          <li className="navbar-item" onClick={handleAboutChulaClick}>About ChulaAvatar</li>
+          <li className="navbar-item">MANGOS</li>
+          <li className="navbar-item">Contact Us</li>
+        </ul>
+      </div>
 
-                    <div className="avatar-icons">
-                        <img src={Avatar1} alt="Avatar 1" className="avatar-icon" />
-                        <img src={Avatar2} alt="Avatar 2" className="avatar-icon" />
-                        <img src={Avatar3} alt="Avatar 3" className="avatar-icon" />
-                        <img src={Avatar4} alt="Avatar 4" className="avatar-icon" />
-                    </div> 
-                </div>
+      {/* Welcome text and avatar icons */}
+      <div className="welcome-container">
+        <h1 className="welcome-h1">Welcome to ChulaAvatar</h1>
+        <p className="welcome-p">
+          Experience the top-notch technology with ChulaAvatar. You can create your lifelike-3D Avatar
+          with one click of your selfie camera.
+        </p>
 
-                <div className="generate-button-container"> 
-                    <button onClick={handleButtonClick} className="welcome-generate-avatar">Create Avatar</button>
-                </div>
+        <div className="avatar-icons">
+          <img src={Avatar1} alt="Avatar 1" className="avatar-icon" />
+          <img src={Avatar2} alt="Avatar 2" className="avatar-icon" />
+          <img src={Avatar3} alt="Avatar 3" className="avatar-icon" />
+          <img src={Avatar4} alt="Avatar 4" className="avatar-icon" />
+        </div>
 
-                <div> 
-                    <div className="centerimage">
-                        <img src= {centerimage} alt="Centered" />
-                    </div>
-                </div>
-                <div className ="bottom-left-button-container">
-                    <button onClick={handleButtonClick} className="welcome-generate-avatar">
-                        Create Avatar
-                    </button>
-                </div>
+        {/* Centered button */}
+        <div className="generate-button-container">
+          <button onClick={handleButtonClick} className="welcome-generate-avatar">
+            Create Avatar
+          </button>
+        </div>
+      </div>
 
-                <div>
-                    <div className="avatar5-container">
-                        <img src={avatar5} alt="Right side" />
-                    </div>
-                </div>
+     
+      <div className="main-content-container">
+        <div className="centerimage">
+          <img src={centerimage} alt="Centered" />
+        </div>
 
-            </div>
-        </>
-    );
+      
+        <div className="bottom-left-button-container">
+          <button onClick={handleButtonClick} className="welcome-generate-avatar">
+            Create Avatar
+          </button>
+        </div>
+
+         <div className='info-text-container'>
+            <h2>3D Human Avatar Creation for Metaverse</h2>
+            <p>This paper presents the process of creating 3D human avatars from reconstructed 3D face produced by PRNet model.</p>
+        </div>
+
+        <div className="avatar5-container">
+          <img src={avatar5} alt="Avatar 5" />
+        </div>
+      
+        
+      </div>
+    </div>
+  );
 }
-export default WelcomePage
+
+export default WelcomePage;
